1d871799b55bb6393d5ba6c7b240ae57565e7742
icedoe <icedoe@live.se>
2016-03-30 14:11:12 +0200

    Update toPDF.sh

--- toPDF.sh
+++ toPDF.sh
@@ -1,8 +1,3 @@
 #! /bin/bash
-<<<<<<< HEAD
-pandoc "$1" -s --template reportToPdf.latex -o report.pdf --bibliography references.bib
-evince report.pdf
-=======
 pandoc "$1".md -s --template reportToPdf.latex -o "$1".pdf --bibliography references.bib
 evince "$1".pdf
->>>>>>> 3a01107b2505b5cad3b56d660634f18bbc7e25e3
